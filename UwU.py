bf0d03f3cac152b7c795b5f88993a1919194a928
RoGamxr <92226973+TTVRoGamxr@users.noreply.github.com>
2023-03-31 10:20:43 -0700

    Update UwU.py

--- UwU.py
+++ UwU.py
@@ -1,3 +1,3 @@
-LatestVersionData = {1: "1.5d", 2: "1.0r"}
+LatestVersionData = {1: "1.5d", 2: "1.0d"}
 global UpdateData
 UpdateData["LatestVersion"] = LatestVersionData[UpdateData["ScriptVersion"]]
