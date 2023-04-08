d137eab85cfd2c482c1e5dbf724ee7e6456d1bf8
RoGamxr <92226973+TTVRoGamxr@users.noreply.github.com>
2023-04-08 15:46:47 -0700

    Update UpdateVersion.py

--- UpdateVersion.py
+++ UpdateVersion.py
@@ -1,3 +1,3 @@
-LatestVersionData = {1: "1.5d", 2: "1.4.4"}
+LatestVersionData = {1: "1.5d", 2: "1.4.5"}
 global UpdateData
 UpdateData["LatestVersion"] = LatestVersionData[UpdateData["ScriptVersion"]]
